0d81a7011b05faa0e0b9cfa25b5d1c6e225181fe
ishowdarkside <ajdin.work@gmail.com>
2025-01-18 19:27:34 +0100

    optionally determine color based on users OS

--- components/ui/PizzaTranslator.tsx
+++ components/ui/PizzaTranslator.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, SectionList, StyleSheet, Text, View } from "react-native";
+import { Button, SectionList, StyleSheet, Text, View, Platform } from "react-native";
 
 const dataModel = { text: "HELLO WORLD" };
 
@@ -11,7 +11,7 @@ const PizzaTranslator = () => {
       marginTop: 50,
     },
     item: {
-      color: "red",
+      color: Platform.OS === "ios" ? "purple" : "blue",
       fontSize: 14,
     },
     heading: {
@@ -23,6 +23,29 @@ const PizzaTranslator = () => {
   const SectionListDataModel = [
     { title: "Header 1", data: ["nova jutra", "kad te snenu", "probude"] },
     { title: "Header 2", data: ["a s desne strane", "mene vise ne bude"] },
+    { title: "Header 3", data: ["nova jutra", "kad te snenu", "probude"] },
+    { title: "Header 4", data: ["a s desne strane", "mene vise ne bude"] },
+    { title: "Header 5", data: ["nova jutra", "kad te snenu", "probude"] },
+    { title: "Header 6", data: ["a s desne strane", "mene vise ne bude"] },
+    { title: "Header 7", data: ["nova jutra", "kad te snenu", "probude"] },
+    { title: "Header 8", data: ["a s desne strane", "mene vise ne bude"] },
+
+    { title: "Header 9", data: ["nova jutra", "kad te snenu", "probude"] },
+    { title: "Header 11", data: ["a s desne strane", "mene vise ne bude"] },
+    { title: "Header 12", data: ["nova jutra", "kad te snenu", "probude"] },
+    { title: "Header 13", data: ["a s desne strane", "mene vise ne bude"] },
+    { title: "Header 14", data: ["nova jutra", "kad te snenu", "probude"] },
+    { title: "Header 15", data: ["a s desne strane", "mene vise ne bude"] },
+    { title: "Header 16", data: ["nova jutra", "kad te snenu", "probude"] },
+    { title: "Header 17", data: ["a s desne strane", "mene vise ne bude"] },
+    { title: "Header 18", data: ["nova jutra", "kad te snenu", "probude"] },
+    { title: "Header 19", data: ["a s desne strane", "mene vise ne bude"] },
+    { title: "Header 20", data: ["nova jutra", "kad te snenu", "probude"] },
+    { title: "Header 21", data: ["a s desne strane", "mene vise ne bude"] },
+    { title: "Header 22", data: ["nova jutra", "kad te snenu", "probude"] },
+    { title: "Header 23", data: ["a s desne strane", "mene vise ne bude"] },
+    { title: "Header 24", data: ["nova jutra", "kad te snenu", "probude"] },
+    { title: "Header 25", data: ["a s desne strane", "mene vise ne bude"] },
   ];
 
   const increaseElementCount = () => setData((curr) => [...curr, dataModel]);
